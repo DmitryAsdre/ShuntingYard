f3422473f586847c85f062208f3499ed09aa7740
AsdreDmitry <michalych2014@yandex.ru>
2021-01-08 15:36:33 +0300

    maybe done

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,4 +1,41 @@
 package com.company;
 
+import javax.management.relation.RelationNotFoundException;
+import java.util.Vector;
+import java.util.Stack;
+
 public class Calculator {
+    private static Vector<Token> getPostfixNotation(String s){
+        return ShuntingYard.getPostfixNotation(s);
+    }
+    public static double calculateExpr(String s){
+        if(s.length() == 0)
+            throw new RuntimeException("unexpected input");
+        Vector<Token> postfix_notation = getPostfixNotation(s);
+        Stack<Double> stack = new Stack<Double>();
+        for(Token token : postfix_notation){
+            if(token.isNum())
+                stack.push(token.getNum());
+            else if(token.isOperator()){
+                Double cur = stack.pop();
+                Double prev = stack.pop();
+                Double res = new Double(0.0);
+                if(token.isMul())
+                    res = cur * prev;
+                else if(token.isDiv()){
+                    if(cur == 0.0)
+                        throw new RuntimeException("division by zero");
+                    res = prev / cur;
+                }
+                else if(token.isPlus())
+                    res = prev + cur;
+                else if(token.isMinus())
+                    res = prev- cur;
+                stack.push(res);
+            }else
+                throw new RuntimeException("unknown token");
+        }
+        assert stack.size() != 1 : "stack size is not 1";
+        return stack.pop();
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,14 @@
 package com.company;
-import java.util.Vector;
-import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Vector<Token> output = ShuntingYard.getPostfixNotation("1 + (3*3 - 4):2");
-        for(Token i : output){
-            System.out.println(i.toString());
+        String s = args[0];
+        try {
+            Double output = Calculator.calculateExpr(s);
+            System.out.println(output);
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
         }
-        //Stack<Token> stack = new Stack<Token>();
-        //stack.push(new Token(Tokens.BEGIN));
-        //stack.push(new Token(Tokens.END));
-        //System.out.println(stack.toString());
     }
 }
